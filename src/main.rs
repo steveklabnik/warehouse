d566851adba6339794663f1a053bc157fe2e9c2f
Steve Klabnik <steve@steveklabnik.com>
2015-08-28 15:42:59 -0400

    Hello world, now with a router

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 extern crate iron;
 
+#[macro_use]
+extern crate router;
+
+use router::Router;
+
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
-    Iron::new(|req: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http("localhost:3000").unwrap();
+    let router = router!(get  "/" => index);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+}
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello world!")))
 }
