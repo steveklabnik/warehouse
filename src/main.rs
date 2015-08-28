1ddbeadc4f7c316d75f170c233676dd9ac77ba78
Steve Klabnik <steve@steveklabnik.com>
2015-08-28 15:34:12 -0400

    Hello, world

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|req: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("localhost:3000").unwrap();
+}
