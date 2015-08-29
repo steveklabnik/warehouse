637e2918f5b05123e615e0e89ccae1c91b3a9ff7
ag_dubs <ashley666ashley@gmail.com>
2015-08-28 21:00:33 -0400

    add deps for bootstrap styling

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -8,7 +8,10 @@ module.exports = function(defaults) {
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
-  //
+
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
