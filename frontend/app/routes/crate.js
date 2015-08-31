3ef2bdb99ed79a0d7e8893a9f956bddd08d9e55a
Manuel Wiedenmann <manuel@funkensturm.de>
2015-08-31 17:35:33 +0200

    remove reload

--- frontend/app/routes/crate.js
+++ frontend/app/routes/crate.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.findRecord('crate', params.crate_id, { reload: true });
+    return this.store.findRecord('crate', params.crate_id);
   }
 });
